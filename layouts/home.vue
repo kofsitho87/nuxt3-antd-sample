1912c36fd06517a64f419473b9f3498982acb62e
kofsitho <kofsitho@gmail.com>
2022-10-27 17:07:23 +0900

    Initial commit

---
+++ layouts/home.vue
@@ -0,0 +1,14 @@
+<script type='ts' setup>
+</script>
+
+<template>
+  <a-layout theme="light">
+    <NuxtPage />
+  </a-layout>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div>
+    <h1 class="text-lg font-semibold">Home</h1>
+
+    <img src="~/assets/comingsoon.jpg" class="w-full object-cover"/>
+  </div>
+</template>
